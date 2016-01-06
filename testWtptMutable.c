a24db38c46bef61f2bbd68f269c5d64c38d490a6
Naowak <yannisbendi@hotmail.Fr>
2016-01-06 16:32:35 +0100

    removeWtptMutable done

--- testWtptMutable.c
+++ testWtptMutable.c
@@ -6,10 +6,18 @@ int main(int argc, char** argv){
 	printWtpt(w);
 	insertWtptMutable(w, 12, 0);
 	insertWtptMutable(w, 96, 4);
-	insertWtptMutable(w, 104, 25);
-	insertWtptMutable(w, 205, 35);
+	//insertWtptMutable(w, 104, 25);
+	//insertWtptMutable(w, 205, 35);
 
 	printf("\n\n");
 	printWtpt(w);
+
+	removeWtptMutable(w, 0);
+	removeWtptMutable(w, 4);
+	removeWtptMutable(w, 2);
+
+	printf("\n\n");
+	printWtpt(w);
+
 	return 0;
 }
--- wtptMutable.c
+++ wtptMutable.c
@@ -21,7 +21,6 @@ void insertWtptMutable(Wtpt w, TYPE c, int pos){
 		assert(w != NULL);
 
 		bit = (code >> i) & 1; //bit = 1 ou 0
-		printf("pos : %d, taille : %d\n", pos, getNumberOfElemWtpt(w));
 		insertBitmapMutable(getBitmapWtpt(w), bit, pos);
 		setNumberOfElemWtpt(w, getNumberOfElemWtpt(w) + 1);
 		
@@ -50,6 +49,48 @@ void insertWtptMutable(Wtpt w, TYPE c, int pos){
 				}
 				w = getLeftSonWtpt(w);
 			}
+	}
+	setAllHighWtpt(tmp);
+}
+
+/* 
+	/!\ ATTENTION
+	On ne prends pas en compte le fait qu'un wtpt atteigne 0 elem à l'intérieur.
+	On devrait le supprimer si le cas présent arrive.
+	Lui et tous ses fils.
+*/
+void removeWtptMutable(Wtpt w, int pos){
+	assert(w != NULL);
+	assert(pos < getNumberOfElemWtpt(w));
+	assert(getNumberOfElemWtpt(w) > 0);
+
+	int codeSize = getCodeSizeDict(getDictWtpt(w));
+
+	Wtpt tmp = w;
+
+	int bit;
+	int i;
+	for(i = 0; i < codeSize; i++){
+		bit =  (getWord(getBitmapWtpt(w), pos/32) >> pos) & 1;
+		removeBitmapMutable(getBitmapWtpt(w), pos);
+		setNumberOfElemWtpt(w, getNumberOfElemWtpt(w) - 1);
+
+		if(pos != 0)
+			/* pos devient le nombre de "bit" qu'il y avait avant lui
+			On fait attention, car on ne peut pas utiliser rankB pour 0
+			si pos est 0, finalement on reste à 0 :) */
+			pos = rankB(getBitmapWtpt(w), pos, bit);
+
+		if(i+1 < codeSize)
+			if(bit)
+				//bit == 1 donc droite
+				w = getRightSonWtpt(w);
+			else
+				//bit == 0 donc gauche
+				w = getLeftSonWtpt(w);
+
+		if(w == NULL)
+			break;
 	}
 	setAllHighWtpt(tmp);
 }
